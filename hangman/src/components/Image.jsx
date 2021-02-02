f91897f71a19b9ce96e62c6248843bcf2e3ef0f2
“Othman <othmanosx@gmail.com>
2021-02-02 20:02:23 +0300

    added hangman image

--- hangman/src/components/Image.jsx
+++ hangman/src/components/Image.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import hangmanImg from '../hangman.png';
+import './hangman.css'
 
 export default class Image extends Component {
   render() {
-    return <div></div>;
+    return <div><img className='hangman' src={hangmanImg} alt="hangman" /></div>;
   }
 }
