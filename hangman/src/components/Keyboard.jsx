3b629b88a393a6b6111f25794b36762ae85cd3df
Abdulazeez <abdulazeezalabbasi@yahoo.com>
2021-02-02 21:39:04 +0300

    keyboard functionality

--- hangman/src/components/Keyboard.jsx
+++ hangman/src/components/Keyboard.jsx
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
+import '../../App.css';
 
 export default class Keyboard extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            mistakes: 0,
+            guessed: new Set([]),
+            clickedBefore:false,
+        }
+    }
+    handleClick=e => {
+        let letter = e.target.value;
+        this.setState(st => ({
+          guessed: st.guessed.add(letter),
+        }))};
+    
   render() {
-    return <div></div>;
+    const letters = [
+      "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+      "t","u","v","w","x","y","z",
+    ];
+    const keyboard = letters.map((letter)=>
+    <button className="buttons"
+    onClick={this.handleClick}
+    disabled={this.state.guessed.has(letter)}
+    
+    value={letter}>
+        {letter}
+    </button>)
+    return (
+      <div>
+        <h1>Keyboard</h1>
+        {keyboard}
+      </div>
+    );
   }
 }
