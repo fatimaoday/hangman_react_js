da05179d14af4e324592e7ea29e1091dbbd57039
“Othman <othmanosx@gmail.com>
2021-02-02 21:20:50 +0300

    finished hangman images

--- hangman/src/components/Image.jsx
+++ hangman/src/components/Image.jsx
@@ -1,9 +1,52 @@
 import React, { Component } from "react";
-import hangmanImg from '../hangman.png';
+import hangman0 from './hangman images/hangman0.png';
+import hangman1 from './hangman images/hangman1.png';
+import hangman2 from './hangman images/hangman2.png';
+import hangman3 from './hangman images/hangman3.png';
+import hangman4 from './hangman images/hangman4.png';
+import hangman5 from './hangman images/hangman5.png';
+import hangman6 from './hangman images/hangman6.png';
 import './hangman.css'
-
+let mistakes = 0
 export default class Image extends Component {
   render() {
-    return <div><img className='hangman' src={hangmanImg} alt="hangman" /></div>;
+    return <div>
+      {(() => {
+        switch (mistakes) {
+          case 0:
+            return (
+              <img className='hangman' src={hangman0} alt="hangman" />
+            )
+          case 1:
+            return (
+              <img className='hangman' src={hangman1} alt="hangman" />
+            )
+          case 2:
+            return (
+              <img className='hangman' src={hangman2} alt="hangman" />
+            )
+          case 3:
+            return (
+              <img className='hangman' src={hangman3} alt="hangman" />
+            )
+          case 4:
+            return (
+              <img className='hangman' src={hangman4} alt="hangman" />
+            )
+          case 5:
+            return (
+              <img className='hangman' src={hangman5} alt="hangman" />
+            )
+          case 6:
+            return (
+              <img className='hangman' src={hangman6} alt="hangman" />
+            )
+          default:
+            return (
+              <img className='hangman' src={hangman0} alt="hangman" />
+            )
+        }
+      })()}
+    </div>;
   }
 }
