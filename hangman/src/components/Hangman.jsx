707552fff6810e7e42010a7a7c2250406d0c23df
Abdulazeez <abdulazeezalabbasi@yahoo.com>
2021-02-05 09:42:42 +0300

    keyboard buttons fully functional

--- hangman/src/components/Hangman.jsx
+++ hangman/src/components/Hangman.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Image from "./Image";
-import '../App.css';
+import "../App.css";
 // import Keyboard from "./Keyboard";
 const axios = require("axios");
 
@@ -11,10 +11,8 @@ export default class Hangman extends Component {
     this.state = {
       word: "",
       counter: 0,
-      guessed: []
+      guessed: [],
     };
-
-    
   }
 
   componentDidMount() {
@@ -24,73 +22,64 @@ export default class Hangman extends Component {
         this.setState({
           word: result.data[0],
           counter: 0,
-          guessed: []
+          guessed: [],
         })
       )
       .catch((error) => console.log(error));
   }
 
   guessedWord() {
-    return this.state.word.split("").map(letter => {
-      if (this.state.guessed.includes(letter)) return letter
-      else return " _ "
+    return this.state.word.split("").map((letter) => {
+      if (this.state.guessed.includes(letter)) return letter;
+      else return " _ ";
     });
   }
 
-  handleGuess = e => {
+  handleGuess = (e) => {
     // get the values of the clicked letter an
-    let value = "e"
+    let value = e.target.value;
     if (this.state.word.includes(value)) {
       this.setState({
-        guessed: [...(this.state.guessed), value],
+        guessed: [...this.state.guessed, value],
       });
     } else {
       this.setState({
-        counter: this.state.counter + 1
+        counter: this.state.counter + 1,
+        guessed: [...this.state.guessed, value]
       });
     }
-
-
-  }
-
-  resetButton = () => {
-    axios
-      .get("https://random-word-api.herokuapp.com/word?number=1")
-      .then((result) =>
-        this.setState({
-          word: result.data[0],
-          counter: 0,
-          guessed: new Set([])
-        })
-      )
-      .catch((error) => console.log(error));
   };
 
-  
+  resetButton = () => {
+    this.componentDidMount();
+  };
 
-  generateButtons(){
+  generateButtons= () => {
     const letters = [
-      "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
-      "t","u","v","w","x","y","z",
+      "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+      "p","q","r","s","t","u","v","w","x","y","z",
     ];
-    const keyboard = letters.map((letter)=>
-    <button className="buttons"
-    onClick={this.handleGuess}
-    // disabled={this.state.guessed.has(letter)}
-    value={letter}>
+    const keys = letters.map((letter) => (
+      
+      <button
+        className="buttons"
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.includes(letter)}
+        value={letter}
+      >
         {letter}
-    </button>)
+      </button>
+    ));
     return (
       <div>
-        <h1>Keyboard</h1>
-        {keyboard}
+        
+        {keys}
       </div>
     );
-  }
-    
-    
+  };
+
   render() {
-    const keyboard = this.generateButtons()
+    const keyboard = this.generateButtons();
     return (
       <div>
         <h1>Hangman Game</h1>
@@ -99,11 +88,19 @@ export default class Hangman extends Component {
         </div>
         <div>
           <p>Guess the Word:</p>
-          <p>{!(this.state.counter >= 6) ? this.guessedWord() : this.state.word}</p>
-          {(this.guessedWord().join("") === this.state.word) ? "YOU WON !! " : null}
-          {(this.state.counter >= 6) ? "YOU LOST !!" : <p>{keyboard}</p>}
+          <p>
+            {!(this.state.counter >= 6) ? this.guessedWord() : this.state.word}
+          </p>
+          {this.guessedWord().join("") === this.state.word
+            ? "YOU WON !! "
+            : null}
+          {this.state.counter >= 6 ? (
+            "YOU LOST !!"
+          ) : (
+            <p>{keyboard}</p>
+          )}
           <br />
-          <button onClick={this.resetButton}>Reset</button>
+          <button className ="buttons" onClick={this.resetButton}>Play Again</button>
         </div>
       </div>
     );
